ec628bf455125c4e8cdaef722fd2360e282a06d3
Anta Huang <anta.huang@gmail.com>
2015-04-29 16:02:57 +0800

    start the lang8 scrapy project

---
+++ lang8/lang8/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for lang8 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'lang8'
+
+SPIDER_MODULES = ['lang8.spiders']
+NEWSPIDER_MODULE = 'lang8.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'lang8 (+http://www.yourdomain.com)'
