85fb1e89eb315bba282c8e7b4eb7c9e631e5a761
Anta Huang <anta.huang@gmail.com>
2015-05-06 16:13:20 +0800

    remove html formatted content in response

--- lang8/lang8/spiders/lang8_spider.py
+++ lang8/lang8/spiders/lang8_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 from scrapy import Selector
 from scrapy.http import FormRequest
 from scrapy.contrib.spiders import CrawlSpider, Rule
@@ -35,14 +36,25 @@ class Lang8Spider(CrawlSpider):
 		self.log('parsing %s' % response.url)
 		item = Lang8Item()
 		hxs = Selector(response)
-		#item['main'] = hxs.select('//div[@id="body_show_ori"]/text()').extract()
 		correction_list = hxs.xpath('//div[@class="correction_box"]')
+		to_be_remove = ["<p>", "</p>", "<span class=\"f_blue\">", "</span>"]  # words in blue are going to be kept
+		to_be_compile = [r"<span class=\"f_red\">(.*?)</span>",
+						 r"<span class=\"sline\">(.*?)</span>",
+						 r"<span class=\"f_gray\">(.*?)</span>"
+						]
+		compiled=[]
+		for one in to_be_compile:
+			compiled.append(re.compile(one))
 		if correction_list:
 			for co in correction_list:
 				correct_xpath = co.xpath('.//li[@class="corrected correct"]')
 				if not correct_xpath:
 					continue
-				incorrect = co.xpath('.//li[@class="incorrect"]/text()').extract()
-				correct = correct_xpath.extract()
-				self.log('correct=%s incorrect=%s' % (correct, incorrect))
+				incorrect = co.xpath('.//li[@class="incorrect"]/text()').extract()[0]
+				correct = correct_xpath.xpath('.//p[1]').extract()[0]
+				for one in compiled:
+					correct = re.sub(one, "", correct)
+				for remove in to_be_remove:
+					correct = correct.replace(remove, "")
+				self.log('correct=%s\nincorrect=%s' % (correct, incorrect))
 		return item
